f702f5b4f16881215f7b259a64b5ce131579377e
Samuel <sam.rodr.g@gmail.com>
2024-06-15 20:13:43 -0300

    Fix icons

--- crates/shared/src/res.rs
+++ crates/shared/src/res.rs
@@ -1,8 +1,8 @@
 //! Embedded resources.
 
-pub static ICON_SMALL: &[u8] = include_bytes!("../../../res/icon/small.png");
+pub static ICON_SMALL: &[u8] = include_bytes!("../../../res/icon/64x64.png");
 
-pub static ICON_MEDIUM: &[u8] = include_bytes!("../../../res/icon/medium.png");
+pub static ICON_MEDIUM: &[u8] = include_bytes!("../../../res/icon/256x256.png");
 
 #[cfg(feature = "release")]
 pub fn licenses() -> Vec<zng::third_party::LicenseUsed> {
