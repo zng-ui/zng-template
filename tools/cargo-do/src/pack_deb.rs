b013c46f69c1d83f0c5acb055f7cff4b21b2b3d6
Samuel <sam.rodr.g@gmail.com>
2024-06-16 00:22:15 -0300

    More deb work

--- tools/cargo-do/src/pack_deb.rs
+++ tools/cargo-do/src/pack_deb.rs
@@ -4,6 +4,7 @@ use crate::util::*;
 use std::{fs, io, path::Path};
 
 pub fn depends() -> String {
+    // fake staging to call pkg-shlibdeps
     let temp: &Path = "target/tmp/do/pack/deb_deps".as_ref();
     let _ = fs::remove_dir_all(&temp);
     (|| {
@@ -15,6 +16,7 @@ pub fn depends() -> String {
     })()
     .ok_or_die("cannot create temp dir");
 
+    // call pkg-shlibdeps
     let exe = Path::new("target/release/t-app-t").canonicalize().unwrap();
     let exe = exe.to_string_lossy();
     let stdout = cmd("dpkg-shlibdeps", &["-O", &exe])
@@ -23,6 +25,7 @@ pub fn depends() -> String {
         .success_or_die("dpkg-shlibdeps run failed");
     let _ = fs::remove_dir_all(&temp);
 
+    // parse package dependencies
     let last_line = stdout.lines().rev().next().unwrap_or_default();
     match last_line.strip_prefix("shlibs:Depends=") {
         Some(deps) => deps.to_owned(),
