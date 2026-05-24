91e70372189debf9f3738f9293cd69533b5d4beb
Samuel Guerra <sam.rodr.g@gmail.com>
2026-05-24 14:02:26 -0300

    Test

--- crates/shared/build.rs
+++ crates/shared/build.rs
@@ -15,6 +15,7 @@ fn pack_l10n() {
     let res_dir =
         std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap()).join("pack-l10n");
     let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("pack-l10n");
+
     std::process::Command::new("cargo")
         .arg("zng")
         .arg("res")
@@ -24,4 +25,11 @@ fn pack_l10n() {
         .expect("failed to pack l10n resources");
     println!("cargo::rerun-if-changed=pack-l10n");
     println!("cargo::rerun-if-changed=../res/l10n");
+
+    if std::env::var("CARGO_CFG_TARGET_OS")
+        .unwrap_or_default()
+        .contains("android")
+    {
+        panic!("show stdout");
+    }
 }
