bce41aaf781755e917692cf8b451e9c4b18df33f
Samuel Guerra <sam.rodr.g@gmail.com>
2026-05-24 15:44:46 -0300

    Don't allow "shared" build that fails to pack

--- crates/shared/build.rs
+++ crates/shared/build.rs
@@ -12,24 +12,19 @@ fn main() {
 #[cfg(feature = "release")]
 /// Pack l10n dir for embedding using `l10N.load_tar`.
 fn pack_l10n() {
+    println!("cargo::rerun-if-changed=pack-l10n");
+    println!("cargo::rerun-if-changed=../res/l10n");
+
     let res_dir =
         std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap()).join("pack-l10n");
     let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("pack-l10n");
 
-    std::process::Command::new("cargo")
+    let cargo_zng_res = std::process::Command::new("cargo")
         .arg("zng")
         .arg("res")
         .arg(res_dir)
         .arg(out_dir)
         .status()
         .expect("failed to pack l10n resources");
-    println!("cargo::rerun-if-changed=pack-l10n");
-    println!("cargo::rerun-if-changed=../res/l10n");
-
-    if std::env::var("CARGO_CFG_TARGET_OS")
-        .unwrap_or_default()
-        .contains("android")
-    {
-        panic!("show stdout");
-    }
+    assert!(cargo_zng_res.success());
 }
