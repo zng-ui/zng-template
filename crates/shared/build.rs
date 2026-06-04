e9ad8a90adec4760063f843e220420d748c47f3d
Samuel Guerra <sam.rodr.g@gmail.com>
2026-06-03 21:27:58 -0300

    Update with new .zr-l10n and licenses embedding

--- crates/shared/build.rs
+++ crates/shared/build.rs
@@ -1,30 +1,43 @@
 fn main() {
-    // Collect license text for all dependencies
     #[cfg(feature = "release")]
-    {
-        pack_l10n();
-
-        let licenses = zng_tp_licenses::collect_cargo_about("../../.cargo/about.toml");
-        zng_tp_licenses::write_bundle(&licenses);
-    }
+    release::build();
 }
 
 #[cfg(feature = "release")]
-/// Pack l10n dir for embedding using `l10N.load_tar`.
-fn pack_l10n() {
-    println!("cargo::rerun-if-changed=pack-l10n");
-    println!("cargo::rerun-if-changed=../res/l10n");
+mod release {
+    pub fn build() {
+        build_l10n();
+        build_licenses();
+    }
 
-    let res_dir =
-        std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap()).join("pack-l10n");
-    let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("pack-l10n");
+    // Pack l10n dir for embedding using `l10N.load_tar`.
+    fn build_l10n() {
+        println!("cargo::rerun-if-changed=pack-l10n");
+        println!("cargo::rerun-if-changed=../res/l10n");
 
-    let cargo_zng_res = std::process::Command::new("cargo")
-        .arg("zng")
-        .arg("res")
-        .arg(res_dir)
-        .arg(out_dir)
-        .status()
-        .expect("failed to pack l10n resources");
-    assert!(cargo_zng_res.success());
+        let res_dir = std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap())
+            .join("pack-l10n");
+        let out_dir =
+            std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("pack-l10n");
+
+        let cargo_zng_res = std::process::Command::new("cargo")
+            .arg("zng")
+            .arg("res")
+            .arg(res_dir)
+            .arg(out_dir)
+            .status()
+            .expect("failed to pack l10n resources");
+        assert!(cargo_zng_res.success());
+    }
+
+    // Collect license text for all dependencies
+    fn build_licenses() {
+        let licenses = zng_tp_licenses::collect_cargo_about_for(
+            "../../.cargo/about.toml",
+            "../t-app-t/Cargo.toml",
+            "release",
+        );
+        // add extra licenses here
+        zng_tp_licenses::write_bundle(&licenses);
+    }
 }
--- tools/cargo-do-pack/src/util.rs
+++ tools/cargo-do-pack/src/util.rs
@@ -1,34 +1,9 @@
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::fs;
 
 use tools_util::*;
 
-/// Generate .zr-copy instruction files for each [`release_l10n`].
-pub fn l10n() {
-    let copy = release_l10n();
-
-    // generate .zr-copy instruction files so we don't need to implement dir copying
-    if !copy.is_empty() {
-        let target = std::env::var("ZR_TARGET_DD").unwrap_or_die("expected `ZR_TARGET_DD` env var");
-        let target = Path::new(&target).join("l10n");
-        fs::create_dir_all(&target).unwrap_or_die("cannot create target l10n dir");
-
-        for lang_dir in copy {
-            let file_name = lang_dir.file_name().unwrap().to_str().unwrap();
-            let target = target.join(format!("{file_name}.zr-copy"));
-            fs::write(target, lang_dir.display().to_string().as_bytes())
-                .unwrap_or_die("cannot write lang zr-copy");
-        }
-    }
-}
-
-/// Get res/l10n/{name} that are:
-/// - Localized for app, not just ./deps/
-/// - Not pseudo*
-/// - Not template
-pub fn release_l10n() -> Vec<PathBuf> {
+/// Get release langs
+pub fn release_langs() -> Vec<String> {
     let mut r = vec![];
     for lang_dir in fs::read_dir("res/l10n").unwrap_or_die("cannot read res/l10n") {
         let lang_dir = lang_dir.unwrap_or_die("cannot read res/l10n entry").path();
@@ -54,7 +29,7 @@ pub fn release_l10n() -> Vec<PathBuf> {
             }
 
             if has_ftl {
-                r.push(lang_dir)
+                r.push(name.into_owned());
             }
         }
     }
